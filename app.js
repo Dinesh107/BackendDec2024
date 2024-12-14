833bdeece39f654ac23ba13fe7815e4031ea64ff
Dinesh107 <dineshjr107@gmail.com>
2024-12-14 18:08:21 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,184 @@
+// console.log("Welcome to js");
+// window.alert("Welcome to ja");
+
+// document.getElementById("myH1").textContent = "Welcome to js world";
+
+// Varaiables - a container thar stores a value 
+
+// behaves as if it were the value contains
+
+// declaration 
+// let, var, const 
+
+// let x = 10;
+
+
+// console.log(x);
+
+// // assignment 
+
+// number, string, boolean 
+
+// let firstName = 'Gowtham';
+
+// console.log("Hai " + firstName);
+
+// console.log(`hai ${firstName} welcome to js classes`);
+
+// let online = true;
+
+// console.log(`gowtham is online: ${online}`);
+
+// let student = 30;
+
+// student = student + 1;
+
+// student += 1;
+
+// console.log(student);
+
+
+// let x = 1;
+// x++; -> post incrment
+// x--; -> post decrement
+// ++x --> pre 
+// --x --> pre decrement
+// console.log(x++);
+
+// how to accecpt user input 
+
+// easy way 
+
+// professionaL way
+
+// let username;
+
+// username = window.prompt("Whats your username");
+
+// console.log(username);
+
+// let username;
+
+// document.getElementById("myButton").onclick = function() {
+//    username = document.getElementById("myText").value;
+//    document.getElementById("myH1").textContent = `Welcome ${username}`;
+//    console.log(username);
+// }
+
+
+
+// Type Conversion
+
+// let x = "2";
+// console.log(typeof x);
+// let y = 23;
+// console.log(typeof y);
+// let z = true;
+
+// x = Number(x);
+// console.log(typeof x);
+// y = String(y);
+// console.log(typeof y);
+// x = Boolean(x);
+// console.log(typeof x);
+// console.log(x);
+
+// ternary Operator = shortcut to if() and else() statements 
+
+// let age = 10;
+
+// condition ? conIfTrue : conIfFasle;
+
+// let message = age >= 18 ? "You are major" : "You are minor";
+
+// console.log(message);
+
+// Number Guessing Game
+
+// const minNum = 1;
+// const maxNum = 10;
+
+
+// const answer = Math.floor(Math.random() * (maxNum - minNum + 1));
+
+// let attempt = 0;
+// let guess;
+// let running = true;
+
+
+// while(running) {
+  
+//     guess = window.prompt(`Guess a number b/w ${minNum} to ${maxNum}`);
+//     guess = Number(guess);
+//     if(isNaN(guess)) {
+//        window.alert("Please enter the valid number");
+//     } else if(guess < minNum || guess > maxNum) {
+//         window.alert(`Please enter the valid number`);
+//     } else {
+//         attempt++;
+//         if(guess < answer) {
+//             window.alert("Too low, try again");
+//         } else if(guess > answer) {
+//             window.alert("Too high, try again");
+//         } else {
+//             window.alert(`Congratulations, you guessed it in ${attempt} attempts`);
+//             running = false;
+//         }
+//     }
+
+// }
+
+// Variable scope let vs var 
+
+// arrays
+
+// spread operator
+// rest parameters
+// callbacks 
+// ForEach() loop
+// map()
+// filter()
+// reduce()
+// fuction expression
+// arrow function 
+// javascript objects
+// Constructors
+// classess 
+// STATIC keywords
+// getter and setter
+// Destructing 
+// nested objects
+// sorting
+
+
+// setTimeout
+// ErrorHandling 
+// DOM Navigation 
+// Mouse Events 
+// key events 
+
+// Node List 
+// class List
+
+// Async/awiat 
+// promises 
+// json files 
+// fetch data from an API 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
