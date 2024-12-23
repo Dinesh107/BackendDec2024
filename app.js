16f564bdd8fa67f2904844509eeb9158fd175b9e
Dinesh107 <dineshjr107@gmail.com>
2024-12-23 11:32:54 +0530

    fourth commit

--- app.js
+++ app.js
@@ -489,20 +489,781 @@
 //     return element >= 18;
 // }
 
-// reduce()
+// reduce() - reduce the elemnet of an array to single value 
+
+// const prices = [10, 20, 30, 42.4, 50, 60];
+
+// const totalPrice = prices.reduce(sum);
+
+// console.log(`${totalPrice.toFixed(2)}`);
+
+
+// function sum(accumulator, element) {
+//   return accumulator + element;
+// }
+
+// Task
+
+// const grades = [75, 50, 90, 99, 65];
+
+// max and min using reduce
+
+// function declaration - define a reusable block of code that performs a specific task
+
+// function helloStudent() {
+//     console.log("Hello Student");
+// }
+
+// helloStudent();
+
+// function expression -  away to define a function as value or variables
+
+// 1) callbacks in asynchronous operators
+// 2) higher order functions
+// 3) event listeners
+
+// We are not polluting the global name sapce with function names we are only going to be using this function once there no need to declare the function.
+
+// const helloStudent = function() {
+//     console.log("Hello Student");
+// }
+
+// helloStudent();
+
+
+// setTimeout(function() {
+//     console.log("Hello World");
+// }, 3000);
+
+// const numbers = [1, 2, 3, 4, 5, 6];
+
+// const squares = numbers.map(function(element){
+//     return Math.pow(element, 2);
+// });
+
+// console.log(squares);
+
+
+// function square(element) {
+//     return Math.pow(element, 2);
+// }
+
+// arrow functions - consise way to write a function expression, good for simple functions that you use only once.
+
+// function hello() {
+//     console.log("Hello World!!!");
+// }
+
+// const hello = (name, age) => { console.log(`hello ${name}`)
+//                                console.log(`You are ${age}, years old`);};
+
+
+// hello("balaji", 30);
+
+// setTimeout(() => console.log("Hello"), 3000);
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// const squares = numbers.map((element) => Math.pow(element, 2));
+// const evenNums = numbers.filter((element) => element % 2 === 0);
+
+// const total = numbers.reduce((accumulator, element) => accumulator + element);
+
+// console.log(total);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 // fuction expression
 // arrow function 
 // javascript objects
+
+// objects - a collection of releated properties and/or methods can represent real world objects (people, products, places)
+
+// object = {key: value, function()}
+
+// const person1 = {
+//       firstname: "John",
+//       lastname: "Doe",
+//       age: 28,
+//       isemployed: true,
+//       sayHello: function() {console.log("Hai! i am john");
+//       },
+//       sayBye: function() {console.log("Goodbye");
+//       }
+//     };
+
+// const person2 = {
+//       firstname: "Jane",
+//       lastname: "Smith",
+//       age: 34,
+//       isemployed: false,
+//       sayHello: function() {console.log("Hai! i am john");
+//       },
+//       sayBye: function() {console.log("Goodbye");
+//       }
+//     };
+
+// const person3 = {
+//       firstname: "Mike",
+//       lastname: "Johnson",
+//       age: 40,
+//       isemployed: true,
+//       sayHello: function() {console.log("Hai! i am john");
+//       },
+//       sayBye: function() {console.log("Goodbye");
+//       }
+//     };
+
+// const person4 = {
+//       firstname: "Emily",
+//       lastname: "Davis",
+//       age: 25,
+//       isemployed: false,
+//       sayHello: function() {console.log("Hai! i am john");
+//       },
+//       sayBye: function() {console.log("Goodbye");
+//       }
+//     };
+
+
+
+// console.log(person1.firstname);
+// console.log(person2.firstname);
+// console.log(person3.firstname);
+// console.log(person4.firstname);
+
+// const person = { 
+//     firstname: "John",
+//     lastname: "Doe",
+//     age: 28,
+//     isemployed: true,
+//     sayHello() {console.log(`Hai! I am ${this.firstname}`);
+//     }
+// }
+
+// console.log(person.firstname);
+// console.log(person.lastname);
+// person.sayHello();
+
+
+// this = refrence to the object where THIS is used (the object depends on the imediate context)
+// person.name = this.name
+
+// const person1 = {
+//     name: 'John',
+//     favFood: "Dosa",
+//     sayHello: function() {
+//         console.log(`Hi i am ${this.name}` );
+//     },
+//     eat : function() {
+//         console.log(`I am eating ${this.favFood}`);
+//     }
+// }
+// const person2 = {
+//     name: 'adam',
+//     favFood: "idly",
+//     sayHello: function() {
+//         console.log(`Hi i am ${this.name}` );
+//     },
+//     eat : function() {
+//         console.log(`I am eating ${this.favFood}`);
+//     }
+// }
+
+// person2.sayHello();
+// person2.eat();
+
+
+// Constructor = special method for defining the properties and methods of objects.
+
+// const car1 = {
+//     make: "Toyota",
+//     model: "Camry",
+//     year: 2020,
+//     color: "red",
+//     drive: function() {console.log(`You drive the ${this.model}`)}
+// }
+
+
+// const car2 = {
+//     make: "Maruthi",
+//     model: "Swift",
+//     year: 2016,
+//     color: "white",
+//     drive: function() {console.log(`You drive the ${this.model}`)}
+// }
+
+// const car3 = {
+//     make: "Tata",
+//     model: "Punch",
+//     year: 2012,
+//     color: "Grey",
+//     drive: function() {console.log(`You drive the ${this.model}`)}
+// }
+
+// function Car(make, model, year, color) {
+//     this.make = make;
+//     this.model = model;
+//     this.year = year;
+//     this.color = color;
+//     this.drive = function() {
+//         console.log(`You drive the ${this.model}`);
+//     };
+// }
+
+// const car1 = new Car("Toyota", "Camry", 2024, "red");
+// const car2 = new Car("Maruthi", "Swift", 2016, "white");
+// const car3 = new Car("Tata", "Punch", 2012, "grey");
+
+// console.log(car1);
+// car1.drive();
+// console.log(car2);
+// car2.drive();
+// console.log(car3);
+// car3.drive();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Constructors
-// classess 
-// STATIC keywords
+// classes - ES6 Features provide a more structred and clearer way to work with objects compared to the traditional constructor functions 
+
+// eg static, encapsulation, inheritance
+
+// class Product {
+//     constructor(name, price, quantity) {
+//         this.name = name;
+//         this.price = price;
+//         this.quantity = quantity;
+//     }
+//     displayProductInfo() {
+//         console.log(`Name: ${this.name}, Price: ${this.price}, Quantity: ${this.quantity}`);
+//     }
+
+//     calculateTotal(salesTax) {
+//        return this.price + (this.price * salesTax);
+//     }
+
+// }
+
+// const salesTax = 0.05;
+
+// const product1 = new Product("Shirt", 500, 1);
+// console.log(product1);
+// product1.displayProductInfo();
+
+// const product2 = new Product("Shorts", 300, 2);
+// console.log(product2);
+// product2.displayProductInfo();
+
+// const total = product1.calculateTotal(salesTax);
+// console.log(`Total price (with tax): ₹${total.toFixed(2)}`);
+
+
+
+// STATIC keywords - Keyword that defines the properties and methods that belong to a class itself rather than the objects created from the that class (class owns anything, not the objects)
+
+
+// class MathUtil {
+//     static PI = 3.14159;
+
+//     static GetDiaameter(radius) {
+//         return radius * 2;
+//     }
+
+//     static getCircumference(radius) {
+//         return 2 * this.PI * radius;
+//     }
+
+//     static getArea(radius) {
+//         return this.PI * radius * radius;
+//     }
+// }
+
+
+// console.log(MathUtil.PI);
+// console.log(MathUtil.GetDiaameter(10));
+// console.log(MathUtil.getCircumference(10));
+// console.log(MathUtil.getArea(10));
+
+// class User {
+//     static userCount = 0;
+
+//     constructor(username) {
+//         this.username = username;
+//         User.userCount++;
+//     }
+
+//     sayHello() {
+//         console.log(`Hello, my username ${this.username}`)
+//     }
+
+//     static getUserCount() {
+//         console.log(`There are ${User.userCount} users online`);
+        
+//     }
+
+// }
+
+// const user1 = new User("john");
+// const user2 = new User("Thiloth");
+// const user3 = new User("Navin");
+
+
+// user1.sayHello();
+// user2.sayHello();
+// user3.sayHello();
+
+// User.getUserCount();
+
+// console.log(User.userCount);
+
+
+
+// inheritance -  allow a new class to inherit properties and methods from an exiting class (Parent -> Child)
+
+//(help with code reusability)
+
+// class Animals {
+//     alive = true;
+//     eat() {
+//         console.log("I am eating");
+//     }
+//     sleep() {
+//         console.log("I am sleeping");
+//     }
+// }
+
+// class Rabbit extends Animals {
+//     name = "rabbit"
+//     run() {
+//         console.log(`${this.name} is running`);
+//     }
+// }
+// class Fish extends Animals {
+//     name = "fish"
+//     swim() {
+//         console.log(`${this.name} is swimming`);
+//     }
+// }
+// class Parrot extends Animals {
+//     name = "parrot"
+//     fly() {
+//         console.log(`${this.name} is flying`);
+//     }
+// } 
+
+// const rabbit = new Rabbit();
+// const fish = new Fish();
+// const parrot = new Parrot();
+
+
+
+// console.log(rabbit.alive);
+// rabbit.eat();
+// rabbit.sleep();
+// rabbit.run();
+// fish.swim()
+// parrot.fly();
+
+
+
+// super - keyword is used in classes to call the constructor or access the properties and methods of a parent .
+//   this = object
+//   super = the parent
+
+// benefits of using the constructor if there is any property that the clildren all sharing in common we can sed them to the constructorof the parent
+
+// using super keyword we can extend a method from the parent.
+
+
+// class Animals {
+
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+
+//     alive = true;
+//     eat() {
+//         console.log("I am eating");
+//     }
+//     sleep() {
+//         console.log("I am sleeping");
+//     }
+//     move(speed) {
+//         console.log(`The ${this.name} is moves at a speed of ${speed}  mph`);
+//     }
+// }
+
+// class Rabbit extends Animals {
+//     constructor(name, age, runSpeed) {
+//         super(name, age);  // calling parent constructor
+//         this.runSpeed = runSpeed;
+//     }
+//     name = "rabbit"
+//     run() {
+//         console.log(`${this.name} is running`);
+//         super.move(this.runSpeed);
+//     }
+
+
+// }
+// class Fish extends Animals {
+//     constructor(name, age, swimSpeed) {
+//         super(name, age);  // calling parent constructor
+//         this.swimSpeed = swimSpeed;
+//     }
+//     name = "fish"
+//     swim() {
+//         console.log(`${this.name} is swimming`);
+//         super.move(this.swimSpeed);
+//     }
+// }
+// class Parrot extends Animals {
+  
+//     constructor(name, age, flySpeed) {
+//         super(name, age);  // calling parent constructor
+//         this.flySpeed = flySpeed;
+//     }
+
+//     name = "parrot"
+//     fly() {
+//         console.log(`${this.name} is flying`);
+//         super.move(this.flySpeed)
+//     }
+// } 
+
+
+// const rabbit = new Rabbit("Fluffy", 2, 25);
+// const fish = new Fish("nemo", 3, 12);
+// const parrot = new Parrot("ram", 1, 30);
+
+// console.log(rabbit.alive);
+
+// rabbit.eat();
+
+// rabbit.sleep();
+
+// rabbit.run();
+// fish.swim();
+// parrot.fly();
+
 // getter and setter
-// Destructing 
-// nested objects
+
+
+// getter - special method that makes a property readable
+// setters - special method that makes a property writable
+
+
+// validate and modify a value when reading/writing a property 
+
+// class Rectangle {
+//     constructor(width, height) {
+//         this.width = width;
+//         this.height = height;
+//     }
+
+//     set width(newwidth) {
+//         if(newwidth > 0) {
+//             this._width = newwidth;
+//         } else {
+//             console.error("width must be +ve num");
+            
+//         }
+//     }
+//     set height(newheight) {
+//         if(newheight > 0) {
+//             this._height = newheight;
+//         } else {
+//             console.error("height must be +ve num");
+            
+//         }
+//     }
+
+//     get width() {
+//         return `${this._width.toFixed(2)} cm`;
+//     }
+//     get height() {
+//         return `${this._height.toFixed(2)}cm`;
+//     }
+
+//     getArea() {
+//         return (this._width * this._height).toFixed(2);
+//     }
+
+// }
+
+// const rectangle = new Rectangle(3, 4);
+
+
+// console.log(rectangle.width);
+// console.log(rectangle.height);
+// console.log(rectangle.getArea());
+
+
+
+
+
+// Class Shape {
+   
+// }
+
+// child 
+// square() - proprties and methods
+// circle() - proprties and methods
+// cylinder() - proprties and methods
+
+
+
+// class Person {
+//     constructor(firstname, lastname, age) {
+//         this.firstname = firstname;
+//         this.lastname = lastname;
+//         this.age = age;
+//     }
+
+//     set firstname(newFirstName) {
+//         if(typeof newFirstName === "string" && newFirstName.length > 0)  {
+//              this._firstname = newFirstName;
+//         } else {
+//             console.error("Invalid firstname, it must be non-empty string"); 
+//         }
+//     }
+//     set lastname(newLastName) {
+//         if(typeof newLastName === "string" && newLastName.length > 0)  {
+//              this._lastname = newLastName;
+//         } else {
+//             console.error("Invalid LastName, it must be non-empty string"); 
+//         }
+//     }
+
+//     set age(newAge) {
+//         if(typeof newAge === "number" && newAge >= 0)  {
+//              this._age = newAge;
+//         } else {
+//             console.error("Invalid age, it must be a positive num"); 
+//         }
+//     }
+ 
+//     get firstname() {
+//         return this._firstname;
+//     }
+//     get lastname() {
+//         return this._lastname;
+//     }
+//     get age() {
+//         return this._age;
+//     }
+    
+// }
+
+
+// const person = new Person("thiloth", "ram", 28);
+
+// console.log(person.firstname, person.lastname, person.age);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Destructing - extract values from arrays and objects then assign them to variables in a convenient way .
+
+// [] - to perform array destructuring
+
+// {} - to perform object destructuring
+
+
+// swap the values of two variable
+
+// let a = 10;
+
+// let b = 20;
+
+// [a, b] = [b, a];
+
+// console.log(a);
+
+// console.log(b);
+
+
+// swap 2 two elements in array 
+
+// const colors = ["red", "green", "blue", "yellow", "white"];
+
+// [colors[0], colors[4]] = [colors[4], colors[0]];
+
+// console.log(colors);
+
+// assign array elements to variables 
+// that how to assign array elements to varaibles using destrcturing
+
+// const colors = ["red", "green", "blue", "yellow", "white"];
+
+// const [firstColor, secondColor, thirdColor, ...extraColors] = colors;
+
+// console.log(firstColor);
+
+// console.log(secondColor);
+
+// console.log(thirdColor);
+
+// console.log(extraColors);
+
+// Extract values from objects 
+
+// const person1 = {
+//     firstName: 'John',
+//     lastName: 'Doe',
+//     age: 30,
+//     job: "Cook",
+// }
+// const person2 = {
+//     firstName: 'Ram',
+//     lastName: 'siva',
+//     age: 27,
+// }
+
+
+// const {firstName, lastName, age, job} = person1;
+
+// console.log(firstName);
+// console.log(lastName);
+// console.log(age);
+// console.log(job);
+
+// const {firstName, lastName, age, job = "unemployed"} = person2;
+
+// console.log(firstName);
+// console.log(lastName);
+// console.log(age);
+// console.log(job);
+
+
+// Destructure in function parameters 
+
+
+// function displayPerson({firstName, lastName, age, job}) {
+//      console.log(`${firstName} ${lastName}`);
+//      console.log(`${age}`);
+//      console.log(`${job}`);   
+// }
+
+// const person1 = {
+//     firstName: 'John',
+//     lastName: 'Doe',
+//     age: 30,
+//     job: "Cook",
+// }
+// const person2 = {
+//     firstName: 'Ram',
+//     lastName: 'siva',
+//     age: 27,
+// }
+
+
+// displayPerson(person2);
+
+
+// nested objects - objects inside of other object.
+
+// allows us to represent more complex data structures
+//  child object is enclosed by parent object
+
+// person{Address{}, ContactInfo{}}
+// ShoppingCart{Keyboard{}, Mouse{}, Monitor{}}
+
+
+// array of objects 
+
+// const friuts = [{name: "apple", color: "red"}, {}, {}, {}, {}, {}, {}]
+
 // sorting
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // setTimeout
